b293003510cb3d1268792509cb07c66aa1f99934
student <anton_pavlovsky@epam.com>
2019-10-24 09:57:12 +0300

    final correction

--- hometask1.sh
+++ hometask1.sh
@@ -3,5 +3,5 @@
 pyenv install 2.7.0
 pyenv install 3.7.0
 
-pyenv virtualenv testenv_2.7.0
-pyenv virtualenv testenv_3.7.0
+pyenv virtualenv 2.7.0 testenv_2.7.0
+pyenv virtualenv 3.7.0 testenv_3.7.0
