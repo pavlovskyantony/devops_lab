840b8e54efd08ee01df0ca5b440764aaafc97115
student <anton_pavlovsky@epam.com>
2019-10-24 23:57:48 +0300

    commit after Flake

--- task6.py
+++ task6.py
@@ -1,3 +1,3 @@
 a, y = map(int, input().split())
 scores = [map(float, input().split()) for n in range(y)]
-[print(sum(student)/len(student)) for student in zip(*scores)]
+[print(sum(student) / len(student)) for student in zip(*scores)]
